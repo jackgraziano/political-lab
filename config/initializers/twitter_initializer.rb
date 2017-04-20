19514c25a88d368c1f029f4af230bc1f47da5096
Jackson Graziano <jackgraziano@gmail.com>
2017-04-19 23:09:52 -0300

    initial

---
+++ config/initializers/twitter_initializer.rb
@@ -0,0 +1,6 @@
+@client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = "LREH2mP8hlbmAB4jCgufIQLAx"
+  config.consumer_secret     = "816o148NtCUKhBYg7ldC93SwFSyusRr0UVfYvTzN3Ercu5wjVM"
+  config.access_token        = "25763542-X43ywERhaMdkKahoIO7xWX15bP3EOR3mGLhNZOEJQ"
+  config.access_token_secret = "uH5F0UlOAonym63RpozlLHL9teNmQMqLYE6Z95y0SKrON"
+end
